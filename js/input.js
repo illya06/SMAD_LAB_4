ead62b5fe38ef9ae74dcc7d3894d0686414e62b7
Ilya Shevchyk <il2001sh@gmail.com>
2021-04-02 20:24:32 +0300

    for ease of testing

--- js/input.js
+++ js/input.js
@@ -1,11 +1,16 @@
 
-var data = {};
+var data = {
+    1: [0.35, 0.35, 0.30, 0.36, 0.31, 0.36, 0.34, 0.34],
+    2: [0.38, 0.37, 0.38, 0.36, 0.40, 0.36, 0.48, 0.32],
+    3: [0.32, 0.31, 0.36, 0.33, 0.40, 0.42, 0.43, 0.41],
+    4: [0.35, 0.38, 0.42, 0.47, 0.60, 0.91, 0.89, 0.59]
+};
 
 function getInput() {
     for (i = 1; i <= 4; i++) {
         let row = [];
         for (j = 1; j <= 8; j++) {
-            let val = document.getElementById(`${i*10+j}`).value;
+            let val = document.getElementById(`${i * 10 + j}`).value;
             row.push(+val);
         }
         data[`${i}`] = row;
