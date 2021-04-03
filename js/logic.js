d1d94846535015ae284f09be13693ccc0c05e1d4
Ilya Shevchyk <il2001sh@gmail.com>
2021-04-03 13:30:45 +0300

    program`s logic

--- js/logic.js
+++ js/logic.js
@@ -21,29 +21,40 @@ function q(data) {
 
     for (let i = 0; i < 4; i++)
         for (let j = 0; j < 8; j++)
-            res += Math.pow(data[i][j]-x,2)
-        
+            res += Math.pow(data[i][j] - x, 2)
+
     return res
 }
 
 //group dispersion
-function q1(data){
+function q1(data) {
     let xG = xGeneral(data)
-    let res =0
+    let res = 0
 
-    for(let i=0;i<4;i++)
-        res+=Math.pow(x(data[i])-xG, 2)
-    
-    return res*8
+    for (let i = 0; i < 4; i++)
+        res += Math.pow(x(data[i]) - xG, 2)
+
+    return res * 8
 }
 
 //in group dispersion
-function q2(data){
+function q2(data) {
     let res = 0
 
     for (let i = 0; i < 4; i++)
         for (let j = 0; j < 8; j++)
-            res += Math.pow(data[i][j]-x(data[i]),2)
-        
+            res += Math.pow(data[i][j] - x(data[i]), 2)
+
     return res
+}
+
+function comp() {
+    let ret = ''
+    let fisher = (q1(input) / 3) / (q2(input) / 24);
+    if (fisher < tableValue) {
+        ret = `(${fisher.toFixed(2)} < ${tableValue.toFixed(2)} ) -> можна знехтувати`
+    } else {
+        ret = `(${fisher.toFixed(2)} > ${tableValue.toFixed(2)} ) -> впливає на результат`
+    }
+    return ret
 }
